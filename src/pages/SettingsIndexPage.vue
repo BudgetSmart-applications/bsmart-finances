6990ff9ddbf97f7e60a61cae13e7807d9c89510c
William King <billaking@gmail.com>
2023-08-10 18:34:03 -0700

    MVP for registration. user cannot update without a registration key

--- src/pages/SettingsIndexPage.vue
+++ src/pages/SettingsIndexPage.vue
@@ -6,17 +6,47 @@
       <div class="q-ma-md">
         <div class="col-6 section">
           <div class="section-title">General</div>
-          <div class="section-description" color="primary">Categories</div>
-          <p class="text-paragraph">
-            Configure default behavior of users, including registration
-            requirements, e-mails, user avatars.
-          </p>
-          <q-btn color="secondary" :disable="!agree">Check for updates</q-btn>
+          <div class="section-description" color="primary">
+            Check for updates
+          </div>
+          <div class="q-my-md" v-if="!!user.registrationKey">
+            <p class="text-paragraph">
+              <strong>Application version</strong>:
+              {{ user.applicationVersion }}
+            </p>
+            <p class="text-paragraph">
+              <strong>Database version</strong>: {{ user.databaseVersion }}
+            </p>
+            <p class="text-paragraph">
+              <strong>Update available</strong>: {{ user.updateAvailable }}
+            </p>
+            <p class="text-paragraph">
+              You can manually check for updates. If an update is available, you
+              will be prompted to download and install it.
+            </p>
+          </div>
+          <div class="q-my-sm" v-else>
+            Register to receive updates, community support, news and training and more.
+          </div>
+
+          <q-btn color="secondary" :disable="!user.registrationKey"
+            >Check for updates</q-btn
+          >
         </div>
         <div class="col-6 section">
-          <div class="section-title">System</div>
-          <div class="section-description">Site information</div>
-          <p class="text-paragraph">Configure default behavior of tables.</p>
+          <div class="section-title">Tables</div>
+          <div class="section-description">
+            Site wide table size and pagination
+          </div>
+          <p class="text-paragraph">
+            <strong>Pagination settings</strong>: Change how many records you'd
+            like to see. For example, show me 10 records out of 1000.
+          </p>
+          <p class="text-paragraph">
+            <strong>Table height settings</strong>: Change the height of the
+            table. For example, choose dense to see more content on your mobile
+            display.
+          </p>
 
           <div>
             <div>Table pagination</div>
@@ -52,6 +82,46 @@
               :boundary-numbers="true"
               :emit-immediately="false"
             />
+            <div class="q-md-lg">
+              <p>Apply these settings to the following tables:</p>
+              <q-checkbox
+                v-model="tables.transactions"
+                label="Transactions"
+                val="auto"
+                :disable="tableHeight === 'auto'"
+              />
+              <q-checkbox
+                v-model="tables.accounts"
+                label="Accounts"
+                val="auto"
+                :disable="tableHeight === 'auto'"
+              />
+              <q-checkbox
+                v-model="tables.categories"
+                label="Categories"
+                val="auto"
+                :disable="tableHeight === 'auto'"
+              />
+              <q-checkbox
+                v-model="tables.budgets"
+                label="Budgets"
+                val="auto"
+                :disable="tableHeight === 'auto'"
+              />
+              <q-checkbox
+                v-model="tables.goals"
+                label="Goals"
+                val="auto"
+                :disable="tableHeight === 'auto'"
+              />
+              <q-checkbox
+                v-model="tables.reports"
+                label="Reports"
+                val="auto"
+                :disable="tableHeight === 'auto'"
+              />
+            </div>
+            <q-btn class="q-my-md" color="positive">Apply table settings</q-btn>
           </div>
           <div class="q-mt-lg">
             <div class="section-title">Registration</div>
@@ -60,10 +130,95 @@
               Register your copy of the BudgetSmart App to receive updates,
               community support, news and training notifications.
             </p>
-            <q-btn color="primary" @click="showTermsDialog = true">Read license</q-btn>
-            <q-checkbox v-model="agree" label="I agree to the license terms" />
-            <q-card v-if="agree">
-              <q-card-section> </q-card-section>
+            <q-btn color="primary" @click="showTermsDialog = true"
+              >Read license</q-btn
+            >
+            <q-checkbox
+              v-model="agree"
+              label="I agree to the terms of use. I'd like to register."
+            />
+            <q-card dark bordered class="q-my-lg bg-grey-9">
+              <q-card-section class="bg-grey-4">
+                <q-input
+                  bg-color="white" outlined dense
+                  :readonly="!agree"
+                  class="q-my-sm"
+                  label="Name"
+                  v-model="user.name"
+                />
+                <q-input
+                  bg-color="white" outlined dense
+                  :readonly="!agree"
+                  class="q-my-sm"
+                  label="Facebook"
+                  v-model="user.facebook"
+                />
+                <q-input
+                bg-color="white" outlined dense
+                  :readonly="!agree"
+                  class="q-my-sm"
+                  label="Twitter"
+                  v-model="user.twitter"
+                />
+                <q-input
+                bg-color="white" outlined dense
+                  :readonly="!agree"
+                  class="q-my-sm"
+                  label="Website"
+                  v-model="user.website"
+                />
+                <q-input
+                bg-color="white" outlined dense
+                  :readonly="!agree"
+                  class="q-my-sm"
+                  label="Email"
+                  v-model="user.email"
+                />
+                <q-input
+                bg-color="white" outlined dense
+                  :readonly="!agree"
+                  class="q-my-sm"
+                  label="Username"
+                  v-model="user.username"
+                />
+                <q-input
+                bg-color="white" outlined dense
+                  :readonly="!agree"
+                  type="password"
+                  label="Password"
+                  class="q-my-sm"
+                  v-model="user.password"
+                />
+                <q-input
+                bg-color="white" outlined dense
+                  :readonly="!agree"
+                  type="password"
+                  label="Confirm Password"
+                  class="q-my-sm"
+                  v-model="user.password"
+                />
+                <q-input
+                bg-color="white" outlined dense
+                  readonly
+                  class="q-my-sm"
+                  label="Registration Key"
+                  v-model="user.registrationKey"
+                />
+                <q-input
+                bg-color="white" outlined dense
+                  readonly
+                  class="q-my-sm"
+                  label="Database Version"
+                  v-model="user.databaseVersion"
+                />
+              </q-card-section>
+              <q-card-actions align="right">
+                <q-btn
+                  color="primary"
+                  :disable="!!user.registrationKey || !agree"
+                  label="Register"
+                />
+              </q-card-actions>
             </q-card>
           </div>
         </div>
@@ -71,7 +226,7 @@
     </div>
     <q-dialog v-model="showTermsDialog">
       <q-card>
-        <q-card-section> These are the terms of use license. </q-card-section>
+        <q-card-section>These are the terms of use agreement. </q-card-section>
       </q-card>
     </q-dialog>
   </div>
@@ -82,6 +237,27 @@ export default {
   name: "SettingsIndexPage",
   data() {
     return {
+      tables: {
+        transactions: true,
+        accounts: true,
+        categories: true,
+        budgets: true,
+        goals: true,
+        reports: true,
+      },
+      user: {
+        name: "John Doe",
+        facebook: "https://www.facebook.com/johndoe",
+        twitter: "https://www.twitter.com/johndoe",
+        website: "https://www.johndoe.com",
+        email: "john.doe@nomail.com",
+        username: "johndoe",
+        password: "password",
+        registrationKey: "124987452",
+        databaseVersion: "1.0.0",
+        applicationVersion: "0.0.1",
+        updateAvailable: false,
+      },
       pagination: 10,
       tableHeight: "Standard",
       agree: false,
